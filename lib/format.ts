1dba1c134532485baa7b4f381a0e1e2b2bf0eddc
leleca7 <wanelle52@gmail.com>
2026-09-19 21:27:54 -0300

    Add lib/format.ts

---
+++ lib/format.ts
@@ -0,0 +1,15 @@
+export function formatBRL(cents: number) {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(cents / 100);
+}
+
+export function slugify(value: string) {
+  return value
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/(^-|-$)/g, "");
+}
